e460d7a955bf24758133b529cc943bc4746a8744
Charles Proust <konoha33@hotmail.fr>
2023-12-20 23:29:58 +0100

    add users, assignments, disponibilities - controller, manager, router

---
+++ backend/src/models/AssignmentsManager.js
@@ -0,0 +1,50 @@
+const AbstractManager = require("./AbstractManager");
+
+class assignmentsManager extends AbstractManager {
+  constructor() {
+    super({ table: "employees_assignments" });
+  }
+
+  // C
+  async create({ reservationId }) {
+    const [rows] = await this.database.query(
+      `INSERT INTO ${this.table} (reservation_id) VALUES (?)`,
+      [reservationId]
+    );
+    return rows.insertId;
+  }
+
+  // R
+  async read(id) {
+    const [rows] = await this.database.query(
+      `SELECT * FROM ${this.table} WHERE id = ?`,
+      [id]
+    );
+    return rows[0];
+  }
+
+  async readAll() {
+    const [rows] = await this.database.query(`SELECT * FROM ${this.table}`);
+    return rows;
+  }
+
+  // U
+  async update({ reservationId, employeeId, id }) {
+    const [rows] = await this.database.query(
+      `UPDATE ${this.table} SET reservation_id=?, employee_id=? WHERE id=?`,
+      [reservationId, employeeId, id]
+    );
+    return [rows];
+  }
+
+  // D
+  async delete(id) {
+    const [rows] = await this.database.query(
+      `DELETE FROM ${this.table} WHERE id = ?`,
+      [id]
+    );
+    return [rows];
+  }
+}
+
+module.exports = assignmentsManager;
--- backend/src/router.js
+++ backend/src/router.js
@@ -6,17 +6,35 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
+// Import userControllers module for handling user-related operations
+const usersControllers = require("./controllers/usersControllers");
+const assignmentsControllers = require("./controllers/assignmentsControllers");
+const disponibilitiesControllers = require("./controllers/disponibilitiesControllers");
 
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
+// Route to get a list of users
+router.get("/users", usersControllers.browse);
+router.get("/assignments", assignmentsControllers.browse);
+router.get("/disponibilities", disponibilitiesControllers.browse);
 
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
+// Route to get a specific user by ID
+router.get("/users/:id", usersControllers.read);
+router.get("/assignments/:id", assignmentsControllers.read);
+router.get("/disponibilities/:id", disponibilitiesControllers.read);
 
-// Route to add a new item
-router.post("/items", itemControllers.add);
+// Route to add a new user
+router.post("/users", usersControllers.add);
+router.post("/assignments", assignmentsControllers.add);
+router.post("/disponibilities", disponibilitiesControllers.add);
+
+// Route to edit a user
+router.put("/users/:id", usersControllers.edit);
+router.put("/assignments/:id", assignmentsControllers.edit);
+router.put("/disponibilities/:id", disponibilitiesControllers.edit);
+
+// Route to destroy user
+router.delete("/users/:id", usersControllers.destroy);
+router.delete("/assignments/:id", assignmentsControllers.destroy);
+router.delete("/disponibilities/:id", disponibilitiesControllers.destroy);
 
 /* ************************************************************************* */
 
