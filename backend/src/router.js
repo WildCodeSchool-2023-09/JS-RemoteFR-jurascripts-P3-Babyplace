401a1383bea086150610fc2874f82d305b099f9d
FredP21 <f.pelletier21@gmail.com>
2023-12-20 17:40:01 +0100

    I create the Manager and the controller for table parents, and i confidurate router.

--- backend/src/router.js
+++ backend/src/router.js
@@ -7,16 +7,16 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
 
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
+const parentsControllers = require("./controllers/parentsController");
 
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
+// parents
 
-// Route to add a new item
-router.post("/items", itemControllers.add);
+router.get("/parents", parentsControllers.browse);
+router.get("/parents/:id", parentsControllers.read);
+router.put("/parents/:id", parentsControllers.edit);
+router.post("/parents", parentsControllers.add);
+router.delete("/parents/:id", parentsControllers.destroy);
 
 /* ************************************************************************* */
 
--- backend/src/tables.js
+++ backend/src/tables.js
@@ -3,10 +3,10 @@
 /* ************************************************************************* */
 
 // Import the manager modules responsible for handling data operations on the tables
-const ItemManager = require("./models/ItemManager");
+const ParentsManager = require("./models/ParentsManager");
 
 const managers = [
-  ItemManager,
+  ParentsManager,
   // Add other managers here
 ];
 
