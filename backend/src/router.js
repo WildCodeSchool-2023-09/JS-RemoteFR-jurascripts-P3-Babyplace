68ae5655d51ee558acc9c7072925fcc9b65d2303
Paul-DELESQUES <pldelesques+github@gmail.com>
2023-12-20 17:22:52 +0100

    add model + controller for table structures

--- backend/src/router.js
+++ backend/src/router.js
@@ -7,16 +7,13 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
+const structuresControllers = require("./controllers/stucturesControllers");
 
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
-
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
-
-// Route to add a new item
-router.post("/items", itemControllers.add);
+router.get("/structures", structuresControllers.browse);
+router.get("/structures/:id", structuresControllers.read);
+router.post("/structures", structuresControllers.add);
+router.put("/structures/:id", structuresControllers.edit);
+router.delete("/structures/:id", structuresControllers.destroy);
 
 /* ************************************************************************* */
 
--- backend/src/tables.js
+++ backend/src/tables.js
@@ -3,10 +3,10 @@
 /* ************************************************************************* */
 
 // Import the manager modules responsible for handling data operations on the tables
-const ItemManager = require("./models/ItemManager");
+const StructureManager = require("./models/StructureManager");
 
 const managers = [
-  ItemManager,
+  StructureManager,
   // Add other managers here
 ];
 
