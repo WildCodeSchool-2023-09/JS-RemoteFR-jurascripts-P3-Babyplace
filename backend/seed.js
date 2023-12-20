996e7eeb95d60732d96850de5d7fe30f72f69277
Charles Proust <konoha33@hotmail.fr>
2023-12-20 10:30:46 +0100

    creation schema sql and seed

--- backend/seed.js
+++ backend/seed.js
@@ -3,9 +3,6 @@
 // Load environment variables from .env file
 require("dotenv").config();
 
-// Import Faker library for generating fake data
-const { faker } = require("@faker-js/faker");
-
 // Import database client
 const database = require("./database/client");
 
@@ -19,17 +16,32 @@ const seed = async () => {
 
     // Generating Seed Data
 
-    // Optional: Truncate tables (remove existing data)
-    await database.query("truncate item");
+    // Insert initial data into the database baby_place
+    await database.query("delete from users");
+    queries.push(
+      database.query(
+        "insert into users (email, password, profile, confirmation_inscription, confirmation_date_sent, created_date, last_connection) values ('picotipicota@structure.fr', 'baby', 'Structure', '1', '2024-01-01 00:00:00', '2024-01-01 00:00:00', '2024-01-01 00:00:00')"
+      )
+    );
+    queries.push(
+      database.query(
+        "insert into users (email, password, profile, confirmation_inscription, confirmation_date_sent, created_date, last_connection) values ('papapoule@papaoule.fr', 'papapoule', 'Parent', '1', '2024-01-01 00:00:00', '2024-01-01 00:00:00', '2024-01-01 00:00:00')"
+      )
+    );
 
-    // Insert fake data into the 'item' table
-    for (let i = 0; i < 10; i += 1) {
-      queries.push(
-        database.query("insert into item(title) values (?)", [
-          faker.lorem.word(),
-        ])
-      );
-    }
+    await database.query("delete from structures");
+    queries.push(
+      database.query(
+        "insert into structures (user_id, name, description, address, address_complements, zip_code, city, phone_number, email, activities, welcomes, experiences, prices) values ('1','BabyWilder', 'lorem ipsum', '33 Wild Code Place', '', '33000', 'Bordeaux', '05.56.56.56.56', 'picotipicota@structure.com', 'Promenade Musique Activités déveil', 'Sorties extérieures Repas maison Foyer non fumeur', 'Formation premier secours Formation nesting Pédagogie Montessori', '3.50')"
+      )
+    );
+
+    await database.query("delete from parents");
+    queries.push(
+      database.query(
+        "insert into parents (user_id, first_name, last_name, birth_name, terms_accepted, date_acceptance_terms, marital_status, address, address_complements, zip_code, city, phone_number, email, profession) values ('2','Papa', 'Poule', 'Poule', '1', '2024-01-01 00:00:00', 'Married', '12 impasse de la rue imaginaire', '', '33000', 'Bordeaux', '06.07.08.09.10', 'papapoule@papaoule.fr', 'Papa à domicile')"
+      )
+    );
 
     /* ************************************************************************* */
 
