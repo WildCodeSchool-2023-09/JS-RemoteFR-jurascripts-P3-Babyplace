f24ffc467752d0f63ef2023aaca438fdbc0a0fe2
Paul-DELESQUES <pldelesques+github@gmail.com>
2024-01-01 19:34:35 +0100

    added functionality to hide the sidebar

--- frontend/src/components/structure/ContentTop.jsx
+++ frontend/src/components/structure/ContentTop.jsx
@@ -1,15 +1,26 @@
+import PropTypes from "prop-types";
 import { iconsNavbar } from "../../assets";
 import "../../styles/contentTop.scss";
 
-function ContentTop() {
+function ContentTop({ toggleSidebar }) {
+  const handleToggleSidebar = () => {
+    toggleSidebar();
+  };
   return (
     <div className="main-content-top">
-      <div className="content-top-left">
+      <button
+        type="button"
+        className="content-top-left"
+        onClick={handleToggleSidebar}
+      >
         <img src={iconsNavbar.menu} alt="menu sidebar" />
         <h3 className="content-top-title">Home</h3>
-      </div>
+      </button>
     </div>
   );
 }
 
+ContentTop.propTypes = {
+  toggleSidebar: PropTypes.func.isRequired,
+};
 export default ContentTop;
--- frontend/src/pages/structure/Dashboard.jsx
+++ frontend/src/pages/structure/Dashboard.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import ContentTop from "../../components/structure/ContentTop";
 import Sidebar from "../../components/structure/Sidebar";
 import "../../styles/dashboard.scss";
 
 function Dashboard() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <div className="dashboard-ctn-main">
-      <Sidebar />
-      <ContentTop />
+      {isSidebarOpen && <Sidebar />}
+      <ContentTop toggleSidebar={toggleSidebar} />
     </div>
   );
 }
