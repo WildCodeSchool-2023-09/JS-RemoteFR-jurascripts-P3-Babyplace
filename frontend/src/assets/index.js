d895c88ecd9a5d4304004c22114cd24d7022e0b6
Paul-DELESQUES <pldelesques+github@gmail.com>
2024-01-01 10:33:13 +0100

    add icons and table structure for navbar

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -2,4 +2,22 @@ import logo from "./logo_baby.png";
 import home from "./img_connexion_home.png";
 import connexion from "./connexion_desktop.png";
 
+// icons navbar
+
+import agenda from "./structure/agenda.svg";
+import babyplacePro from "./structure/babyplace_pro.svg";
+import email from "./structure/email.svg";
+import gears from "./structure/gears.svg";
+import reservation from "./structure/reservation.svg";
+
+// ------------------------------------------------------------------------------------------
+
 export { logo, home, connexion };
+
+export const iconsNavbar = {
+  agenda,
+  babyplacePro,
+  email,
+  gears,
+  reservation,
+};
---
+++ frontend/src/constants/dataGen.js
@@ -0,0 +1,10 @@
+import { iconsNavbar } from "../assets";
+
+const navLinks = [
+  { id: 1, title: "Liste des demandes", img: iconsNavbar.reservation },
+  { id: 2, title: "Agenda", img: iconsNavbar.agenda },
+  { id: 3, title: "Email", img: iconsNavbar.email },
+  { id: 4, title: "Compte", img: iconsNavbar.gears },
+];
+
+export default navLinks;
