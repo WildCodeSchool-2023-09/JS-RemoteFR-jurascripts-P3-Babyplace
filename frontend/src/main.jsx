4ec499958b2f7c6f57a8f20422b202a0972f6094
Paul-DELESQUES <pldelesques+github@gmail.com>
2024-01-01 12:42:21 +0100

    added route for dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import App from "./App";
 import NotFound from "./pages/NotFound";
 import StructureConnexion from "./pages/structure/StructureConnexion";
+import Dashboard from "./pages/structure/Dashboard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/pro/connexion" element={<StructureConnexion />} />
+        <Route path="/pro/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
---
+++ frontend/src/pages/structure/Dashboard.jsx
@@ -0,0 +1,11 @@
+import "../../styles/dashboard.scss";
+
+function Dashboard() {
+  return (
+    <div className="dashboard-ctn-main">
+      <h2>HELLO SARENTU</h2>
+    </div>
+  );
+}
+
+export default Dashboard;
