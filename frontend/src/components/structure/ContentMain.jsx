9a11e693f7edb4f312d2be8d30dcf50449ce33aa
Paul-DELESQUES <pldelesques+github@gmail.com>
2024-01-02 11:12:27 +0100

    added filter for buttons based on status

--- frontend/src/components/structure/ContentMain.jsx
+++ frontend/src/components/structure/ContentMain.jsx
@@ -9,7 +9,7 @@ function ContentMain() {
       age: "18mois",
       parentName: "Dan Scott",
       status: "En attente",
-      arrival: "2024-01-01T08:00:00Z",
+      arrival: "2024-01-01T07:00:00Z",
       departure: "2024-01-01T17:00:00Z",
       totalHours: "10h",
       totalPrice: "100€",
@@ -31,8 +31,8 @@ function ContentMain() {
       age: "6mois",
       parentName: "John Doe",
       status: "En attente",
-      arrival: "2024-01-01T08:00:00Z",
-      departure: "2024-01-01T17:00:00Z",
+      arrival: "2024-01-01T09:00:00Z",
+      departure: "2024-01-01T16:00:00Z",
       totalHours: "7h",
       totalPrice: "70€",
     },
@@ -43,11 +43,13 @@ function ContentMain() {
       parentName: "Sarah Doe",
       status: "En attente",
       arrival: "2024-01-01T08:00:00Z",
-      departure: "2024-01-01T17:00:00Z",
+      departure: "2024-01-01T11:00:00Z",
       totalHours: "4h",
       totalPrice: "40€",
     },
   ]);
+
+  const [filter, setFilter] = useState("all");
 
   const handleStatusChange = (cardId, newStatus) => {
     setCards((prevCards) =>
@@ -57,56 +59,89 @@ function ContentMain() {
     );
   };
 
+  const filteredCards = () => {
+    if (filter === "accepted") {
+      return cards.filter((card) => card.status === "Accepté");
+    }
+    if (filter === "rejected") {
+      return cards.filter((card) => card.status === "Refusé");
+    }
+    if (filter === "pending") {
+      return cards.filter((card) => card.status === "En attente");
+    }
+    return cards;
+  };
+
+  const changeFilter = (newFilter) => {
+    setFilter(newFilter);
+  };
+
   return (
     <div className="content-main">
-      {cards.map((card) => (
-        <div className={`card ${card.status}`} key={card.id}>
-          <h3>{card.babyName}</h3>
-          <p>Age: {card.age}</p>
-          <p>Parent: {card.parentName}</p>
-          <p>Status: {card.status}</p>
-          <p>Arrivée: {new Date(card.arrival).toLocaleString()}</p>
-          <p>Départ: {new Date(card.departure).toLocaleString()}</p>
-          <p>Total Heures: {card.totalHours}</p>
-          <p>Total Prix: {card.totalPrice}</p>
-          {card.status === "En attente" && (
-            <>
+      <div className="filters-btn">
+        <button type="button" onClick={() => changeFilter("all")}>
+          Tous
+        </button>
+        <button type="button" onClick={() => changeFilter("pending")}>
+          Demande en Attente
+        </button>
+        <button type="button" onClick={() => changeFilter("accepted")}>
+          Accepté
+        </button>
+        <button type="button" onClick={() => changeFilter("rejected")}>
+          Refusé
+        </button>
+      </div>
+      <div className="cards-ctn">
+        {filteredCards().map((card) => (
+          <div className={`card ${card.status}`} key={card.id}>
+            <h3>{card.babyName}</h3>
+            <p>Age: {card.age}</p>
+            <p>Parent: {card.parentName}</p>
+            <p>Status: {card.status}</p>
+            <p>Arrivée: {new Date(card.arrival).toLocaleString()}</p>
+            <p>Départ: {new Date(card.departure).toLocaleString()}</p>
+            <p>Total Heures: {card.totalHours}</p>
+            <p>Total Prix: {card.totalPrice}</p>
+            {card.status === "En attente" && (
+              <>
+                <button
+                  type="button"
+                  className="btn-accept"
+                  onClick={() => handleStatusChange(card.id, "Accepté")}
+                >
+                  Accepter
+                </button>
+                <button
+                  type="button"
+                  className="btn-reject"
+                  onClick={() => handleStatusChange(card.id, "Refusé")}
+                >
+                  Refuser
+                </button>
+              </>
+            )}
+            {card.status === "Accepté" && (
               <button
                 type="button"
-                className="btn-accept"
-                onClick={() => handleStatusChange(card.id, "Accepté")}
+                className="btn-modify"
+                onClick={() => handleStatusChange(card.id, "modified")}
               >
-                Accepter
+                Modifier
               </button>
+            )}
+            {card.status === "Refusé" && (
               <button
                 type="button"
-                className="btn-reject"
-                onClick={() => handleStatusChange(card.id, "Refusé")}
+                className="btn-modify"
+                onClick={() => handleStatusChange(card.id, "modified")}
               >
-                Refuser
+                Modifier
               </button>
-            </>
-          )}
-          {card.status === "Accepté" && (
-            <button
-              type="button"
-              className="btn-modify"
-              onClick={() => handleStatusChange(card.id, "modified")}
-            >
-              Modifier
-            </button>
-          )}
-          {card.status === "Refusé" && (
-            <button
-              type="button"
-              className="btn-modify"
-              onClick={() => handleStatusChange(card.id, "modified")}
-            >
-              Modifier
-            </button>
-          )}
-        </div>
-      ))}
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
