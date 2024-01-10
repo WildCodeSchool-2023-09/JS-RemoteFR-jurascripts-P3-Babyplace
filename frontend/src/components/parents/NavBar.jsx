fbde04c8be7200964aead7ffe2f7e3e3eadb3cd5
FredP21 <f.pelletier21@gmail.com>
2024-01-10 17:54:43 +0100

    test to fix navbar

--- frontend/src/components/parents/NavBar.jsx
+++ frontend/src/components/parents/NavBar.jsx
@@ -6,23 +6,49 @@ import {
   IoNotifications,
   IoHomeSharp,
 } from "react-icons/io5";
+import { useState } from "react";
 
 function NavBar() {
+  const navbar = [
+    {
+      id: 1,
+      icons: <IoHomeSharp className="icons" />,
+      link: "/parents/connexion",
+    },
+    {
+      id: 2,
+      icons: <IoSearch className="icons" />,
+      link: "/parents/creche",
+    },
+    {
+      id: 3,
+      icons: <IoNotifications className="icons" />,
+      link: "/parents/folders",
+    },
+    {
+      id: 4,
+      icons: <IoPerson className="icons" />,
+      link: "/parents/profile",
+    },
+  ];
+
+  const [activeLinkIndex, setActiveLinkIndex] = useState(0);
+
+  const handleActive = (e) => {
+    setActiveLinkIndex(e);
+  };
   return (
     <nav className="navbar_parents">
-      <Link to="/parents/connexion">
-        <IoHomeSharp className="icons" />
-      </Link>
-      <Link to="/parents/creche">
-        {" "}
-        <IoSearch className="icons" />
-      </Link>
-      <Link to="/parents/folders">
-        <IoNotifications className="icons" />
-      </Link>
-      <Link to="/parents/profile">
-        <IoPerson className="icons" />
-      </Link>
+      {navbar.map((icons) => (
+        <Link
+          to={icons.link}
+          key={icons.id}
+          className={` ${icons.id === activeLinkIndex ? "active" : ""}`}
+          onClick={() => handleActive(icons.id)}
+        >
+          {icons.icons}
+        </Link>
+      ))}
     </nav>
   );
 }
