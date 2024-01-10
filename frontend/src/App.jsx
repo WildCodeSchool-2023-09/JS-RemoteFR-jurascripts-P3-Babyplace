a65f85cb4dc0289b43a3c9b1a597d5ebd6aef2ec
Charles Proust <konoha33@hotmail.fr>
2024-01-10 09:58:54 +0100

    edit main and app for auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return <Outlet />;
 }
 
 export default App;
