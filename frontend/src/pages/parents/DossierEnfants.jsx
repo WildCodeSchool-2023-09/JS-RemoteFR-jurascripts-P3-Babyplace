e14b389550d223b958bd52f48d7c81fbe7c5276a
Sadjoaldi <alsannealdi@outlook.com>
2024-01-24 17:05:25 +0100

    add checkbox edit in 4 files, 2 doc and 2 styles

--- frontend/src/pages/parents/DossierEnfants.jsx
+++ frontend/src/pages/parents/DossierEnfants.jsx
@@ -57,13 +57,23 @@ function DossierEnfants() {
         </div>
         <form action="submit">
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.nom}
-              onChange={() => handlValid("nom")}
-            />
+            <label htmlFor="checkboxNom">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxNom"
+                name="validate"
+                checked={validator.nom}
+                onChange={() => handlValid("nom")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input
               type="text"
               placeholder="Nom"
@@ -72,75 +82,145 @@ function DossierEnfants() {
             />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.prenom}
-              onChange={() => handlValid("prenom")}
-            />
+            <label htmlFor="checkboxPrenom">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxPrenom"
+                name="validate"
+                checked={validator.prenom}
+                onChange={() => handlValid("prenom")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Prénom" />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.dateNaissance}
-              onChange={() => handlValid("dateNaissance")}
-            />
+            <label htmlFor="checkboxdateNaissance">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxdateNaissance"
+                name="validate"
+                checked={validator.dateNaissance}
+                onChange={() => handlValid("dateNaissance")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Date de Naissance" />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.marcheur}
-              onChange={() => handlValid("marcheur")}
-            />
+            <label htmlFor="checkboxmarcheur">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxmarcheur"
+                name="validate"
+                checked={validator.marcheur}
+                onChange={() => handlValid("marcheur")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Marcheur / Non Marcheur" />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.allergies}
-              onChange={() => handlValid("allergies")}
-            />
+            <label htmlFor="checkboxallergies">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxallergies"
+                name="validate"
+                checked={validator.allergies}
+                onChange={() => handlValid("allergies")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Allergies" />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.assurance}
-              onChange={() => handlValid("assurance")}
-            />
+            <label htmlFor="checkboxassurance">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxassurance"
+                name="validate"
+                checked={validator.assurance}
+                onChange={() => handlValid("assurance")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Attestation d'assurance" />
             <RiLinksFill />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.vaccination}
-              onChange={() => handlValid("vaccination")}
-            />
+            <label htmlFor="checkboxvaccination">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxvaccination"
+                name="validate"
+                checked={validator.vaccination}
+                onChange={() => handlValid("vaccination")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Carnet de santé / Vaccination" />
             <RiLinksFill />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.acteNaissance}
-              onChange={() => handlValid("acteNaissance")}
-            />
+            <label htmlFor="checkboxacteNaissance">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxacteNaissance"
+                name="validate"
+                checked={validator.acteNaissance}
+                onChange={() => handlValid("acteNaissance")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input
               type="text"
               placeholder="Acte de naissance / ou certificat de grossesse"
@@ -148,23 +228,43 @@ function DossierEnfants() {
             <RiLinksFill className="validatorIcon" />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.medecinTraitant}
-              onChange={() => handlValid("medecinTraitant")}
-            />
+            <label htmlFor="checkboxmedecinTraitant">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxmedecinTraitant"
+                name="validate"
+                checked={validator.medecinTraitant}
+                onChange={() => handlValid("medecinTraitant")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Médecin traitant" />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.aptitude}
-              onChange={() => handlValid("aptitude")}
-            />
+            <label htmlFor="checkboxaptitude">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxaptitude"
+                name="validate"
+                checked={validator.aptitude}
+                onChange={() => handlValid("aptitude")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input
               type="text"
               placeholder="Certificat d'aptitude à l'acceuil collectif"
@@ -172,13 +272,23 @@ function DossierEnfants() {
             <RiLinksFill />
           </div>
           <div>
-            <input
-              className="check"
-              type="checkbox"
-              name="validate"
-              checked={validator.autorisation}
-              onChange={() => handlValid("autorisation")}
-            />
+            <label htmlFor="checkboxautorisation">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxautorisation"
+                name="validate"
+                checked={validator.autorisation}
+                onChange={() => handlValid("autorisation")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input
               type="text"
               placeholder="Autorisation de soin et d'hospitalisation"
--- frontend/src/pages/parents/DossierInscription.jsx
+++ frontend/src/pages/parents/DossierInscription.jsx
@@ -1,11 +1,29 @@
 import { FaLessThan } from "react-icons/fa6";
-import { GrValidate } from "react-icons/gr";
 import { MdChevronRight } from "react-icons/md";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../../components/parents/NavBar";
 import "../../styles/dossierInscription.scss";
 
 function DossierInscription() {
+  const [validator, setValidator] = useState({
+    justificatifRevenu: false,
+    declaRevenu: false,
+    caf: false,
+    securiteSociale: false,
+    jusDomicile: false,
+    sitProfessionnelle: false,
+    rib: false,
+    autoPhoto: false,
+    autoSortie: false,
+    livretFamille: false,
+    justifDivorce: false,
+  });
+
+  const handlValid = (checkName) => {
+    setValidator((chekcs) => ({ ...chekcs, [checkName]: !chekcs[checkName] }));
+  };
+
   return (
     <section className="InscriptionDoc">
       <div className="parent_box">
@@ -32,7 +50,23 @@ function DossierInscription() {
       <div className="Inscription_doc">
         <form action="submit">
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxjustificatifRevenu">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxjustificatifRevenu"
+                name="validate"
+                checked={validator.justificatifRevenu}
+                onChange={() => handlValid("justificatifRevenu")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input
               type="text"
               placeholder="Justificatif de revenu (moins de 3 moins)"
@@ -40,7 +74,23 @@ function DossierInscription() {
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxdeclaRevenu">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxdeclaRevenu"
+                name="validate"
+                checked={validator.declaRevenu}
+                onChange={() => handlValid("declaRevenu")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input
               type="text"
               placeholder="Déclaration de revenu (année en cours)"
@@ -48,25 +98,86 @@ function DossierInscription() {
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxcaf">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxcaf"
+                name="validate"
+                checked={validator.caf}
+                onChange={() => handlValid("caf")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="number" placeholder="Numéro de la caf" />{" "}
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
-            <input
-              type="number"
-              placeholder="Numéro de sécurité sociale"
-            />{" "}
+            <label htmlFor="checkboxsecuriteSociale">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxsecuriteSociale"
+                name="validate"
+                checked={validator.securiteSociale}
+                onChange={() => handlValid("securiteSociale")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
+            <input type="number" placeholder="Numéro de sécurité sociale" />{" "}
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxjusDomicile">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxjusDomicile"
+                name="validate"
+                checked={validator.jusDomicile}
+                onChange={() => handlValid("jusDomicile")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Justificdati de domicile" />{" "}
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxsitProfessionnelle">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxsitProfessionnelle"
+                name="validate"
+                checked={validator.sitProfessionnelle}
+                onChange={() => handlValid("sitProfessionnelle")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input
               type="text"
               placeholder="Justificatif de situation professionnelles"
@@ -74,30 +185,107 @@ function DossierInscription() {
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxrib">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxrib"
+                name="validate"
+                checked={validator.rib}
+                onChange={() => handlValid("rib")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="RIB" /> <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxautoPhoto">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxautoPhoto"
+                name="validate"
+                checked={validator.autoPhoto}
+                onChange={() => handlValid("autoPhoto")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Autorisation photo et vidéo" />{" "}
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxautoSortie">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxautoSortie"
+                name="validate"
+                checked={validator.autoSortie}
+                onChange={() => handlValid("autoSortie")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Autorisation de sortie" />{" "}
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
+            <label htmlFor="checkboxlivretFamille">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxlivretFamille"
+                name="validate"
+                checked={validator.livretFamille}
+                onChange={() => handlValid("livretFamille")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
             <input type="text" placeholder="Copie livret de famille" />{" "}
             <MdChevronRight />
           </div>
           <div>
-            <GrValidate />
-            <input
-              type="text"
-              placeholder="Copie du jugement de divorce"
-            />{" "}
+            <label htmlFor="checkboxjustifDivorce">
+              {" "}
+              <input
+                type="checkbox"
+                id="checkboxjustifDivorce"
+                name="validate"
+                checked={validator.justifDivorce}
+                onChange={() => handlValid("justifDivorce")}
+              />
+              <svg viewBox="0 0 64 64" height="2em" width="2em">
+                <path
+                  d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                  pathLength="575.0541381835938"
+                  className="path"
+                />
+              </svg>
+            </label>
+            <input type="text" placeholder="Copie du jugement de divorce" />{" "}
             <MdChevronRight />
           </div>
         </form>
